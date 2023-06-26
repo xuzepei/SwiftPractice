eea5c69290712e1ce54980c25c9ebe5e9da61e0e
xuzepei <xuzepei@gmail.com>
2023-06-26 10:57:42 +0800

    no message

---
+++ ScrollViewDemo2/ScrollViewDemo2/ExpandingScrollViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ExpandingScrollViewController.swift
+//  ScrollViewDemo2
+//
+//  Created by xuzepei on 2023/6/26.
+//
+
+import UIKit
+
+class ExpandingScrollViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var expandedView: UIView!
+    @IBOutlet weak var arrow: UIImageView!
+    
+    //@IBOutlet weak var contentViewConstraintHeight: NSLayoutConstraint!
+    @IBOutlet weak var expandedViewConstraintHeight: NSLayoutConstraint!
+    
+    var isExpanded = false
+    var isAnimating = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "Expanding ScrollView"
+        view.backgroundColor = .white
+    }
+
+
+    @IBAction func clickedButton(_ sender: Any) {
+        print("clickedButton")
+        
+        if isAnimating {
+            return
+        }
+        
+        self.isAnimating = true
+        
+        UIView.animate(withDuration: 0.5) {
+            self.arrow.transform = CGAffineTransform(rotationAngle: self.isExpanded ? 0 : -CGFloat.pi / 2 )
+        }
+        
+        //约束的修改要放在动画外
+        self.expandedViewConstraintHeight.constant = self.isExpanded ? 0 : 400
+        
+        //print("self.contentViewConstraintHeight.constant: \(self.contentViewConstraintHeight.constant)")
+        print("self.scrollView.contentSize.height: \(self.scrollView.contentSize.height)")
+        print("self.contentView.height: \(self.contentView.bounds.size.height)")
+        
+        var offsetY:CGFloat = 0
+        //759是contentView在iphone14pro上的设计高度，subview展开后contentView增加250高
+        if self.contentView.bounds.size.height <= (759 + 250) {
+            offsetY = self.isExpanded ? -250 : 250
+            //self.contentViewConstraintHeight.constant = self.contentViewConstraintHeight.constant + offsetY
+        }
+        
+
+        UIView.animate(withDuration: 0.5) {
+            //self.scrollView.contentSize = CGSizeMake(self.scrollView.contentSize.width, self.contentViewConstraintHeight.constant)
+            
+            //print("self.contentViewConstraintHeight.constant: \(self.contentViewConstraintHeight.constant)")
+            print("self.scrollView.contentSize.height: \(self.scrollView.contentSize.height)")
+            print("self.contentView.height: \(self.contentView.bounds.size.height)")
+            
+            //伴随展开View的同时，滚动到View的最下方可视位置
+            self.scrollView.contentOffset = CGPoint(x: self.scrollView.contentOffset.x, y: self.scrollView.contentOffset.y + offsetY)
+            
+            self.view.layoutIfNeeded()
+        } completion: { b in
+
+
+            self.isAnimating = false
+            self.isExpanded = self.isExpanded ? false : true
+            
+
+        }
+
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ScrollViewDemo2/ScrollViewDemo2/MyTableViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MyTableViewController.swift
+//  ScrollViewDemo2
+//
+//  Created by xuzepei on 2023/6/26.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+    
+    
+    var itemArray:[String] = ["ScrollView without xib", "ScrollView without content layout guid", "ScrollView with content layout guid", "ScrollView Expanding"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        self.title = "ScrollView Demo"
+        
+        self.tableView.rowHeight = 60
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 4
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mycell", for: indexPath) as UITableViewCell
+
+        // Configure the cell...
+        
+        cell.textLabel?.text = self.itemArray[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+
+        return cell
+    }
+    
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView .deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row == 0 {
+            let vc = ScrollView1Controller()
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        else if indexPath.row == 1 {
+            let vc = ScrollView2Controller()
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        else if indexPath.row == 2 {
+            let vc = ScrollView3Controller()
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        else if indexPath.row == 3 {
+            let vc = ExpandingScrollViewController()
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        else if indexPath.row == 4 {
+            
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
